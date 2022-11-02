43d575657f7ed9571867f69a18d74b003701493f
vladd12 <svm@asu-vei.ru>
2022-11-02 14:16:11 +0300

    [*] Broken import fixed

--- include/gen/gen_export.h
+++ include/gen/gen_export.h
@@ -5,7 +5,7 @@
 
 #if defined(GEN_EXPORTS)
 #define GENLIB_EXPORT Q_DECL_EXPORT
-#elif defined(Gen_IMPORTS)
+#elif defined(GEN_IMPORTS)
 #define GENLIB_EXPORT Q_DECL_IMPORT
 #else
 #define GENLIB_EXPORT /**/
