1672f41fb7fd9542a8b5eacfc022429f65fe7961
anton <vao@asu-vei.ru>
2023-02-08 17:07:26 +0300

    [*] Conan install dependencies method update

--- cmake/ConanInstallDependencies.cmake
+++ cmake/ConanInstallDependencies.cmake
@@ -1,42 +1,24 @@
-# function for setting target name for build and user directory for conan
+# Downloading conan CMake script and using for autodetecting
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+  message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+  file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+                "${CMAKE_BINARY_DIR}/conan.cmake"
+                EXPECTED_HASH SHA256=5cdb3042632da3efff558924eecefd580a0e786863a857ca097c3d1d43df5dcd
+                TLS_VERIFY ON)
+endif()
+include(${CMAKE_BINARY_DIR}/conan.cmake)
 
-function(conan_install_dependencies TARGET_ARCH TARGET_NAME USER_DIR)
-    # Find conan util
-    find_program(CONAN_FOUND "conan" NO_CACHE)
-    if(CONAN_FOUND)
-        # Define conan's target name and target profile
-        set(CONAN_PROFILE "default")
-        if(${TARGET_ARCH} MATCHES "x86_64")
-            set(CONAN_TARGET_NAME "x86_64")
-            set(CONAN_TARGET_FOUND ON)
-        elseif(${TARGET_ARCH} MATCHES "i386")
-            set(CONAN_TARGET_NAME "x86")
-            set(CONAN_TARGET_FOUND ON)
-        elseif(${TARGET_ARCH} MATCHES "armv7")
-            set(CONAN_TARGET_NAME "armv7hf")
-            set(CONAN_PROFILE "armhf")
-            set(CONAN_TARGET_FOUND ON)
-        else()
-            set(CONAN_TARGET_FOUND OFF)
-        endif()
+# Getting build type, compiler, etc... (other settings)
+conan_cmake_autodetect(settings)
 
-        # If conan's target was defined correctly
-        if(CONAN_TARGET_FOUND)
-            message(STATUS "Conan target: ${CONAN_TARGET_NAME}")
-            message(STATUS "Conan profile: ${CONAN_PROFILE}")
+conan_cmake_configure(
+  REQUIRES xz_utils/5.2.5
+  GENERATORS cmake_find_package
+  OPTIONS xz_utils:shared=False)
 
-            set(CONAN_INSTALL_COMMAND conan install . --profile ${CONAN_PROFILE} -g cmake_find_package -s arch=${CONAN_TARGET_NAME} -s arch_build=${CONAN_TARGET_NAME} -if ${CMAKE_BINARY_DIR} --build=missing)
-            set(ENV{CONAN_USER_HOME} ${USER_DIR})
-            execute_process(
-                COMMAND ${CONAN_INSTALL_COMMAND}
-                RESULT_VARIABLE CMD_ERROR
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-            )
-            message(STATUS "CMD_ERROR:" ${CMD_ERROR})
-        else()
-            message(FATAL_ERROR "Invalid target architecture: ${TARGET_ARCH}")
-        endif()
-    else()
-        message(FATAL_ERROR  "Conan package manager required, please download it from https://conan.io/downloads.html")
-    endif()
-endfunction()
+# Installing libraries
+conan_cmake_install(
+  PATH_OR_REFERENCE .
+  BUILD missing
+  REMOTE conancenter
+  SETTINGS ${settings})
