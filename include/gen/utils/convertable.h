bf7e120d14efeef7eb0ddd152a745fdcee9d3818
vladd12 <vladd12z32@gmail.com>
2024-01-29 10:00:17 +0300

    [+] Adding documenting commentaries

--- include/gen/utils/convertable.h
+++ include/gen/utils/convertable.h
@@ -7,21 +7,30 @@
 namespace utils
 {
 
+/// \brief Class-wrapper for any received QVariant with explicit defined type conversion operators.
 class GENLIB_EXPORT Convertable
 {
 private:
     QVariant m_data;
 
 public:
+    /// \brief Parametrized c-tor with a const reference to the input data.
     explicit Convertable(const QVariant &data) noexcept;
+    /// \brief Parametrized c-tor with rvalue input data.
     explicit Convertable(QVariant &&data) noexcept;
+    /// \brief Copy c-tor.
     Convertable(const Convertable &rhs) noexcept;
+    /// \brief Move c-tor.
     Convertable(Convertable &&rhs) noexcept;
 
+    /// \brief Assignment operator overloading with a const reference.
     Convertable &operator=(const Convertable &rhs) noexcept;
+    /// \brief Assignment operator overloading with a rvalue.
     Convertable &operator=(Convertable &&rhs) noexcept;
 
+    /// \brief Implicit type conversion to bool value.
     operator bool() const noexcept;
+    /// \brief Implicit type conversion to string value.
     operator QString() const noexcept;
 
     operator std::int16_t() const noexcept;
